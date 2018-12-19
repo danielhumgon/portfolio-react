9064a7c8ead7027741d503839786f32c856ec3ba
danielhumgon <danielhumgon@gmail.com>
2018-12-19 05:54:01 +0200

    started about me page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import './../images/avatar.jpg';
 
 class About extends Component {
     state = {  }
     render() {
         return (
 
-            <div>
-                <h1>About Page</h1>
+            <div className="about-grid">
+                <Grid>
+                    <Cell col={4}>
+                        <div className="left-side-div">
+                          <img src={require('./../images/avatar.jpg')} 
+                           alt="Daniel Gonzalez avatar"
+                           className="about-img"
+                          />
+                        </div>
+
+                        <h2>Daniel Gonzalez</h2>
+                        <h4>Web Developer</h4>
+                        <hr />
+                        <p>Lorem Ipsum is simply dummy text of the printing 
+                           and typesetting industry. Lorem Ipsum has been the 
+                           industry's standard dummy text ever since the 1500s
+                        </p>
+                        <hr />
+                        <h5>Address</h5>
+                        <p>some Address</p>
+                        <hr />
+                        <h5>Phone</h5>
+                        <p>(+58)119-1535</p>
+                    </Cell>
+                    <Cell className="about-right-col" col={8}>
+                        right
+                    </Cell>
+                </Grid>
             </div>
             
         );
