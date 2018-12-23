ff8248d1bb58c69d682da3d6b1a73e1e9a0f9293
danielhumgon <danielhumgon@gmail.com>
2018-12-23 07:55:16 +0200

    Updates

---
+++ precache-manifest.e879ed3c2151409b4a56e6ec7aae72b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f700f6703a09125da7eb8c08a564fda0",
+    "url": "/portfolio-react/static/media/contact.f700f670.png"
+  },
+  {
+    "revision": "e6b3a6ea81deb6c3c855aaff911fd2c7",
+    "url": "/portfolio-react/static/media/avatar.e6b3a6ea.jpg"
+  },
+  {
+    "revision": "9338e8a022e42c6bc02f",
+    "url": "/portfolio-react/static/js/runtime~main.9338e8a0.js"
+  },
+  {
+    "revision": "730ff1ca8f626feb878a",
+    "url": "/portfolio-react/static/js/main.730ff1ca.chunk.js"
+  },
+  {
+    "revision": "e5e254f470202ec7755d",
+    "url": "/portfolio-react/static/js/1.e5e254f4.chunk.js"
+  },
+  {
+    "revision": "730ff1ca8f626feb878a",
+    "url": "/portfolio-react/static/css/main.da482047.chunk.css"
+  },
+  {
+    "revision": "e5e254f470202ec7755d",
+    "url": "/portfolio-react/static/css/1.9889b54d.chunk.css"
+  },
+  {
+    "revision": "9ce22fb0e7f39d6864c6d6f85dcc0cfd",
+    "url": "/portfolio-react/index.html"
+  }
+];
